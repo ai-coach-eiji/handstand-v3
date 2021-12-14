82582d13667eb3eddb015f2ee3b5dbd95d6bd929
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-12-14 19:35:44 +0900

    modified env files

--- pose2/settings.py
+++ pose2/settings.py
@@ -141,7 +141,7 @@ DATABASES = {
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASSWORD'),
         'HOST': env('DATABASE_HOST'),
-        'PORT': 5432
+        'PORT': 5432,
     }
 }
 
