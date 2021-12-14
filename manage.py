38660a22e19ede8c68a62371ae986cd4eb48f790
ai-coach-eiji <zy.lefthage@i.softbank.jp>
2021-12-14 19:59:52 +0900

    set the default of asgi file and deleted the settings dir

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pose2.settings.local")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pose2.settings.local")
 
 
 def main():
